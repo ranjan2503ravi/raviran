3404d102c1d8fdb05bf421b56d8eed98510d6e87
ranjan2503ravi <ranjan2503ravi@gmail.com>
2025-01-09 17:10:06 +0530

    first commit

--- java script/hh.js
+++ java script/hh.js
@@ -1,140 +1,47 @@
-// for(let i = 1; i<=5; i++){
-//     console.log("app");
-// }
-// let sum = 0;
-// for(let i = 1; i<=5;i++){
-// sum=sum+1
-// console.log(sum);
-// }
+const don = [
+    {
+        img:"https://i.pinimg.com/736x/2a/ab/bf/2aabbf29be7ed8696dd1b1c39ba6e226.jpg",
+        name: "radhika rajput",
+        email: "radhika@gmail.com",
+        phone:7979056132,
+    },
+    {
+        img:"https://i.pinimg.com/736x/2a/ab/bf/2aabbf29be7ed8696dd1b1c39ba6e226.jpg",
+        name: "radhika rajput",
+        email: "radhika@gmail.com",
+        phone:7979056132,
+    },
+    {
+        img:"https://i.pinimg.com/736x/2a/ab/bf/2aabbf29be7ed8696dd1b1c39ba6e226.jpg",
+        name: "radhika rajput",
+        email: "radhika@gmail.com",
+        phone:7979056132,
+    },
+    {
+        img:"https://i.pinimg.com/736x/2a/ab/bf/2aabbf29be7ed8696dd1b1c39ba6e226.jpg",
+        name: "radhika rajput",
+        email: "radhika@gmail.com",
+        phone:7979056132,
+    },
+    {
+        img:"https://i.pinimg.com/736x/2a/ab/bf/2aabbf29be7ed8696dd1b1c39ba6e226.jpg",
+        name: "radhika rajput",
+        email: "radhika@gmail.com",
+        phone:7979056132,
+    },
+];
+const mom = document.querySelector(".main");
 
-// let n = 10;
-// for(let i =1;i<=n;i++){
-//     console.log(i);
-// }
+don.forEach((idx)=>{
+    const non = document.createElement("div");
+    non.innerHTML = `
+     <div class="box1">
+         <img src="${idx.img}" alt="">
+<p>${idx.name}</p>
+<p>${idx.email}</p>
+<p>${idx.phone}</p>
 
-// --------------------------------------------------------------------------------------
-
-
-//do while loop
-// let n = 10;
-// let a = 1;
-// do{
-//     console.log(a);
-//     a++;
-// }while(a<=n);
-// --------------------------------------------------------------------------------------
-// let n = 10;
-// let a = 1;
-// while(a<=n){
-//     console.log(a);
-//     a++;
-// }
-// --------------------------------------------------------------------------------------
-
-//while loop
-// let a = 0;
-// while(a<=n){
-//     console.log(first);
-//     a++
-// }
-
-// *************/**
-// switch
-
-// let day = "E";
-
-// switch(day){
-//     case "A" : console.log("sun");
-//     break;
-//     case "B" : console.log("mon");
-//     break;
-//     case "C" : console.log("tue");
-//     break;
-//     case "D" : console.log("thu");
-//     break;    
-//     default: console.log("Error")
-// }
-// ---------------------------------------------------------------------
-//array
-// let hero =[12,13,14,15,];
-// console.log(hero);
-// let a = ["ravi","raju","rero","mon",];
-// console.log(a);
-
-
-// -----------------
-// array indices
-//console ke ander (varible name--hero[12]--index-0 or 2 or 3)...etc
-
-// let b =[12,13,14,15,17,16,256];
-// b[2]=96
-// console.log(b.length);
-
-// looping over an arry-----------------------------
-// let ram = [12,13,14,15,16,17,18,19];
-
-// for(let index = 0; index<8;index++){
-//     console.log(ram);
-// }
-
-
-// -----------------------------
-// array method--
-// push():add to end
-//pop():delete from end to return
-//tistring():converts array to string
-//push means areay ke end mai aad karana
-// let item = ["tomato","banana","mango","orange"];
-// item.push("rango");
-// item.shift("mango");
-// item.pop()
-
-// console.log(item);
-
-
-// var a = 13245
-// console.log(typeof a)
-// console.log(typeof a.toString());
-
-//-------------------------------------------------------------------------------
-//static and dinamic
-//non para meter
-// function bond(){
-//     console.log("monagsh");
-// }
-// bond();
-// bond();
-// parameter function mai argument pass karte hai
-// function sum(a,b){
-//     console.log(a+b);
-// }
-// sum(1,3);
-//------
-// function sum(a,b){
-//     console.log(a+b);
-    
-
-// }
-// let x= sum(5,5);
-// console.log(x);
-///-------------------------------------------------------------------
-// function atm(card, amount){
-// return ` nike ${card} , Rs ${amount}`
-// }
-
-
-// let a = atm("shoes", 1065454560);
-// console.log(a)
-///-------------------------------------------------------------------
-
-
-//  function atm(card, amount){
-// return {card, amount}
-// }
-
-// let a = atm("visa", 100); //anomination function
-// console.log(a)
-
-//------------------------------------------------------------------
-
-
+    </div>
+    `;
+    mom.appendChild(non);
+})
